5e0191863e59e8925ce6f56120fa804facfb7ada
Kim Maida <1884525+kmaida@users.noreply.github.com>
2025-07-17 10:38:24 -0400

    update all homepages to remove how it works explanation (moving to readmes)

--- bff-hb/frontend-ng/src/app/pages/HomePage.ts
+++ bff-hb/frontend-ng/src/app/pages/HomePage.ts
@@ -37,59 +37,7 @@ import {Component} from '@angular/core';
     </ul>
 
     <h2>How BFF-HB Authentication Works</h2>
-    <p>Here are the steps for authentication in this BFF-HB example. Most of the authentication lifecycle is handled by the <a href="https://github.com/FusionAuth/fusionauth-javascript-sdk/tree/main/packages/sdk-angular" target="_blank">FusionAuth Angular SDK</a> and <a href="https://fusionauth.io/docs/apis/hosted-backend" target="_blank">Hosted Backend</a>.</p>
-    <ol>
-      <li>
-        User navigates to the frontend app
-      </li>
-      <li>
-        SDK checks if the user is logged in and if so, restores the user's authenticated state (this does not require any additional code)
-      </li>
-      <li>
-        If not authenticated, user clicks the <code>Log In</code> button
-      </li>
-      <li>
-        SDK redirects the user to the authorization server's <code>/oauth2/authorize</code> endpoint with appropriate configuration
-      </li>
-      <li>
-        User authenticates and SDK manages the PKCE code flow exchange behind the scenes
-      </li>
-      <li>
-        Authorization server sets <code>httpOnly</code> cookies for the access token, refresh token (requires <code>offline_access</code> scope), ID token (requires <code>openid</code> scope), and access token expiration
-      </li>
-      <li>
-          If configured with <code>shouldAutoRefresh: true</code>, SDK manages proactive session renewal and refreshes the access token before it expires
-      </li>
-      <li>
-        When the user clicks the <code>Log Out</code> button, the SDK redirects to the authorization server's <code>/oauth2/logout</code> endpoint with appropriate configuration
-      </li>
-      <li>
-        Authorization server logs the user out and redirects to the frontend home page
-      </li>
-      <li>
-        SDK deletes cookies and user is unauthenticated
-      </li>
-    </ol>
-
-    <h2>How BBOC External Resource Server Authorization Works</h2>
-    <p>Cookies only work on the same domain. Therefore, cross-domain requests are proxied to the API. The app makes a request to the API proxy with <code>include: 'credentials'</code> to attach cookies. The app should protect routes that require authentication before permitting navigation to the page that will call the secure API. Checks like this improve the user experience.</p>
-    <ol>
-      <li>
-        Authenticated user navigates to a protected app route that calls a secure, external API proxy
-      </li>
-      <li>
-          App makes a request to the resource API for protected data with <code>include: 'credentials'</code> to attach the access token cookie (<code>app.at</code>) as authorization (for example, to the <code>http://localhost:4200/api/recipe</code> proxy endpoint)
-      </li>
-      <li>
-        Resource server verifies the access token in the <code>app.at</code> cookie and returns data to the app if the token is valid
-      </li>
-      <li>
-        If the user isn't logged in or there's a problem with the access token, the resource server returns a <code>401: Unauthorized</code> error, which is displayed in the app
-      </li>
-    </ol>
-
-    <h2>Other Auth Architectures</h2>
-      <p>FusionAuth provides a hosted backend for browser-based apps that serves as an authentication Backend-for-Frontend. This is an easy-to-implement serverless architecture. The three recommended authentication and authorization architecture choices for browser-based apps are <a href="https://datatracker.ietf.org/doc/html/draft-ietf-oauth-browser-based-apps#name-backend-for-frontend-bff" target="_blank">Backend-for-Frontend</a>, <a href="https://datatracker.ietf.org/doc/html/draft-ietf-oauth-browser-based-apps#name-token-mediating-backend" target="_blank">Token-Mediating Backend</a>, and <a href="https://datatracker.ietf.org/doc/html/draft-ietf-oauth-browser-based-apps#name-browser-based-oauth-20-clie" target="_blank">OAuth 2.0 for Browser-Based Applications</a>. Each architecture has different trade-offs and benefits. Demos of all three architectures are included in this repo.</p>
+    <p>See the <a href="https://github.com/kmaida/auth-architecture-ng/blob/main/bff-hb/README.md#how-bff-hb-authentication-works" target="_blank">GitHub README</a> for a detailed explanation of this BFF-HB pattern implementation.</p>
   `,
 })
 export class HomePage { /* Your component code goes here */ }
